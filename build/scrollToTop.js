aa00fe9f4bb5f639e2b79ac11e28329e934d4890
ediri <engin.diri@gmail.com>
2015-12-17 12:44:22 +0000

    build

--- build/scrollToTop.js
+++ build/scrollToTop.js
@@ -1,7 +1,7 @@
 /*! 
  * angular-scroll-to-top v0.0.1
  * http://ediri.github.io/hover-scroll-to-top
- * Copyright (c) 2015 12:43:36 ediri
+ * Copyright (c) 2015 12:44:22 ediri
  * License: Apache-2.0
  */
 /*
